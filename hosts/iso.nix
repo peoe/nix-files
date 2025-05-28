2507ddb461385514b6c4872982db2d8f3d84e282
Peter Oehme <oehme.pb@gmail.com>
2025-05-28 21:59:27 +0200

    [iso] add user back

--- hosts/iso.nix
+++ hosts/iso.nix
@@ -6,11 +6,12 @@
 
     networking.hostName = "nixos-iso";
 
-    # users.users.nixos = {
-    #     initialPassword = "password";
-    #     isNormalUser = true;
-    #     extraGroups = [ "wheel" ];
-    #     openssh.authorizedKeys.keys = [ vars.sshPublicKeyHomeserver ];
-    # };
-    users.users.root.openssh.authorizedKeys.keys = [vars.sshPublicKeyHomeserver];
+    users.users.nixos = {
+        initialPassword = "password";
+        isNormalUser = true;
+        extraGroups = [ "wheel" ];
+        openssh.authorizedKeys.keys = [ vars.sshPublicKeyHomeserver ];
+    };
+
+    security.sudo.wheelNeedsPassword = false;
 }
