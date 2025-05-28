7184da92d1701da2a7edb571767f74e64c58f592
Peter Oehme <oehme.pb@gmail.com>
2025-05-28 21:43:12 +0200

    [iso] use root directly on remote

--- hosts/iso.nix
+++ hosts/iso.nix
@@ -1,4 +1,4 @@
-{ config, vars, ... }: {
+{ vars, ... }: {
     imports = [
         ./../packages/base.nix
         ./../packages/install.nix
@@ -6,12 +6,11 @@
 
     networking.hostName = "nixos-iso";
 
-    users.users.nixos = {
-        initialPassword = "password";
-        isNormalUser = true;
-        extraGroups = [ "wheel" ];
-        openssh.authorizedKeys.keys = [ vars.sshPublicKeyHomeserver ];
-    };
-
-    security.sudo.wheelNeedsPassword = false;
+    # users.users.nixos = {
+    #     initialPassword = "password";
+    #     isNormalUser = true;
+    #     extraGroups = [ "wheel" ];
+    #     openssh.authorizedKeys.keys = [ vars.sshPublicKeyHomeserver ];
+    # };
+    users.users.root.openssh.authorizedKeys.keys = vars.sshPublicKeyHomeserver;
 }
