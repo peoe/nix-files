6a93d6f482675f99f17985fd03cf1aa19326466a
Peter Oehme <oehme.pb@gmail.com>
2025-05-29 12:11:36 +0200

    [nixserver] add luks initrd support

--- hosts/nixserver/config.nix
+++ hosts/nixserver/config.nix
@@ -30,6 +30,7 @@
     # boot.kernelParams = [ "ip=localaddr::gateway:mask:hostname:interface:off:1.1.1.1:8.8.8.8:" ];
     boot.initrd = {
         availableKernelModules = ["r8169"];
+        luks.forceLuksSupportInInitrd = true;
         network = {
             enable = true;
             flushBeforeStage2 = true;
