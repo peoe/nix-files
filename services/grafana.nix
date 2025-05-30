220ebab937d8d4bd8876d237357c396ff4bd6209
Peter Oehme <oehme.pb@gmail.com>
2025-05-31 00:38:23 +0200

    [grafana] firewall and remove secrets for now

--- services/grafana.nix
+++ services/grafana.nix
@@ -1,14 +1,21 @@
-{ config, vars, ... }: {
+{ config, vars, ... }: let
+    grafanaport = 3000;
+in {
+    networking = {
+        firewall = {
+            allowedTCPPorts = [ grafanaport 80 443 ];
+        };
+    };
+
     services.grafana = {
         enable = true;
         domain = "graphs.lab-leman";
-        port = 3000;
+        port = grafanaport;
         addr = "127.0.0.1";
         
         settings = {
             security = {
                 admin_user = vars.userName;
-                admin_password = config.sops.secrets.grafanapasswd.path;
             };
         };
     };
