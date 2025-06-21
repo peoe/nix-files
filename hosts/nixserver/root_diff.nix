dedd1b23e8d58c95638a207781d8077b0c619afc
Peter Oehme <oehme.pb@gmail.com>
2025-06-21 20:46:47 +0200

    [impermanence] user script for diffing root

---
+++ hosts/nixserver/root_diff.nix
@@ -0,0 +1,30 @@
+{ pkgs, ... }: {
+    environment.systemPackages = with pkgs; [
+        (
+            writeShellScriptBin "root-diff"
+            ''
+            #!/usr/bin/env bash
+            sudo mkdir -p /mnt
+            sudo mount "/dev/mapper/crypted" /mnt -o subvol=/
+            set -euo pipefail
+            OLD_TRANSID=$(sudo btrfs subvolume find-new /mnt/root-snapshot 9999999)
+            OLD_TRANSID=${OLD_TRANSID}
+            sudo btrfs subvolume find-new "/mnt/root" "$OLD_TRANSID" |
+            sed '$d' |
+            cut -f17- -d' ' |
+            sort |
+            uniq |
+            while read path; do
+              path="/$path"
+              if [ -L "$path" ]; then
+                : # The path is a symbolic link, so is probably handled by NixOS already
+              elif [ -d "$path" ]; then
+                : # The path is a directory, ignore
+              else
+                echo "$path"
+              fi
+            done
+            ''
+        )
+    ];
+}
