231a046168fa746b450de23ab8c270fa48f558b7
Peter Oehme <oehme.pb@gmail.com>
2025-06-22 13:22:53 +0200

    [install] add smaller install to allow incremental install

---
+++ hosts/minimal/config.nix
@@ -0,0 +1,99 @@
+{ config, inputs, lib, outputs, pkgs, vars, ... }: {
+    imports = [
+        inputs.home-manager.nixosModules.home-manager
+        inputs.impermanence.nixosModules.impermanence
+        
+        ./../nixserver/disk-config.nix
+
+        ./../../packages/base.nix
+        ./../../packages/nixserver.nix
+        ./../../packages/secrets.nix
+    ];
+
+    networking.hostName = "nixserver";
+
+    users.users.${vars.userName} = {
+        isNormalUser = true;
+        hashedPasswordFile = config.sops.secrets.userpasswd.path;
+        extraGroups = [ "wheel" ];
+        openssh.authorizedKeys.keys = [ vars.sshPublicKeyHomeserver ];
+        shell = pkgs.zsh;
+    };
+
+    home-manager = {
+        extraSpecialArgs = {inherit inputs outputs vars;};
+        useGlobalPkgs = true;
+        useUserPackages = true;
+        users.${vars.userName} = {
+            home = {
+                username = vars.userName;
+                stateVersion = config.system.stateVersion;
+            };
+
+            imports = [
+                inputs.impermanence.homeManagerModules.impermanence
+
+                ./../../programs/default.nix
+            ];
+        };
+    };
+
+    environment.persistence."/persist" = {
+        directories = [
+            "/var/lib/nixos"
+        ];
+
+        files = [
+            "/etc/machine-id"
+            "/etc/ssh/ssh_host_ed25519_key"
+            "/etc/ssh/ssh_host_ed25519_key.pub"
+            "/etc/ssh/ssh_host_rsa_key"
+            "/etc/ssh/ssh_host_rsa_key.pub"
+        ];
+    };
+
+    boot.loader = {
+        systemd-boot = {
+            enable = true;
+            configurationLimit = 5;
+        };
+        efi.canTouchEfiVariables = true;
+        timeout = 5;
+    };
+
+    # boot remote unlock
+    boot.initrd = {
+        availableKernelModules = ["r8169"];
+        luks.forceLuksSupportInInitrd = true;
+
+        network = {
+            enable = true;
+            flushBeforeStage2 = true;
+            ssh = {
+                enable = true;
+                shell = "/bin/cryptsetup-askpass";
+                authorizedKeys = config.users.users.alfred.openssh.authorizedKeys.keys;
+                hostKeys = [ "/nix/secret/initrd/ssh_host_ed25519_key" ];
+            };
+        };
+
+        # try to ensure that we wait for network device before continuing
+        preLVMCommands = lib.mkOrder 400 "sleep 5";
+
+        # erase impermanent files
+        postResumeCommands = lib.mkAfter ''
+        MNTPOINT=$(mktemp -d)
+        mount "/dev/mapper/crypted" "$MNTPOINT" -o subvol=/
+
+        if [[ -e "$MNTPOINT/root" ]]; then
+            mv "$MNTPOINT/root" "$MNTPOINT/old_root"
+        fi
+
+        btrfs subvolume delete "$MNTPOINT/old_root"
+        btrfs subvolume create "$MNTPOINT/root"
+        umount "$MNTPOINT"
+        '';
+    };
+
+    system.stateVersion = "25.05";
+}
