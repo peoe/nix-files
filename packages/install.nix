f865883bbc82f8e9b3450d2231d7a072218bc6cf
Peter Oehme <oehme.pb@gmail.com>
2025-06-22 13:45:29 +0200

    [nixserver] revert storage location of hostkey for initrd

--- packages/install.nix
+++ packages/install.nix
@@ -9,6 +9,7 @@
 
             # format, set luks, mount
             sudo nix run github:nix-community/disko/latest -- --mode destroy,format,mount ~/nix-files/hosts/nixserver/disk-config.nix --yes-wipe-all-disks
+            sudo mkdir -pv /mnt/nix/secret/initrd
             sudo mkdir -pv /mnt/persist/etc/ssh
             sudo mkdir -pv /mnt/persist/var/lib
 
@@ -17,6 +18,7 @@
 
             # initrd hostkey and sops key
             sudo ssh-keygen -t ed25519 -N "" -C "" -f /mnt/persist/etc/ssh/ssh_host_ed25519_key
+            sudo cp /mnt/persist/etc/ssh/ssh_host_ed25519_key /mnt/nix/secret/initrd
             sudo nix-shell --extra-experimental-features flakes -p ssh-to-age --run 'cat /mnt/persist/etc/ssh/ssh_host_ed25519_key.pub | ssh-to-age'
             echo -e "Remember to update .sops.yaml, and call"
             echo ""
