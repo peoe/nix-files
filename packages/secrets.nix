6adac626fc8b2857b736832db5a011ae23affaac
Peter Oehme <oehme.pb@gmail.com>
2025-07-04 11:13:26 +0200

    [secrets] add luks-key

--- packages/secrets.nix
+++ packages/secrets.nix
@@ -5,8 +5,17 @@
     sops = {
         defaultSopsFile = ./../secrets/secrets.yaml;
         age.sshKeyPaths = [ "/nix/secret/initrd/ssh_host_ed25519_key" ];
+
+        # get user password for boot
         secrets.userpasswd.neededForUsers = true;
         secrets.userpasswd = {};
+
+        # get luks-key file for unlocking data drives on boot
+        secrets."luks-key" = {
+            format = "binary";
+            sopsFile = ./../secrets/luks-key;
+        };
+
         gnupg.sshKeyPaths = [];
     };
 }
