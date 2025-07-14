f9f0996ec0649715c4dcd0eb5ceb5f2dd1771bdb
Peter Oehme <oehme.pb@gmail.com>
2025-07-14 15:54:40 +0200

    [data] snapraid and cron movers

--- packages/datadisks.nix
+++ packages/datadisks.nix
@@ -1,4 +1,4 @@
-{ config, ... }: {
+{ config, pkgs, ... }: {
     # crypttab to luksOpen at boot from decryption key
     environment.etc.crypttab = {
         mode = "0600";
@@ -64,6 +64,85 @@
         options = [
             "defaults"
             "noatime"
+        ];
+    };
+
+    services.snapraid = {
+        enable = true;
+        parityFiles = [
+            # must NOT be on the data disks
+            "/mnt/disks/parity1/snapraid.parity"
+        ];
+        contentFiles = [
+            "/mnt/cache/.snapraid.parity"
+            "/mnt/disks/parity1/.snapraid.parity"
+            "/mnt/disks/data1/.snapraid.parity"
+            "/persist/.snapraid.parity"
+        ];
+        dataDisks = [
+            cache = "/mnt/cache"
+            data1 = "/mnt/disks/data1"
+        ];
+        sync.interval = "04:00";
+        sync.interval = "daily";
+        exclude = [
+            "lost+found"
+            ".DS_Store"
+            ".Thumbs.db"
+            "*.unrecoverable"
+        ];
+    };
+
+    environment.systemPackages = with pkgs; [
+        (
+            writeShellScriptBin "mergerfs_time_based_mover"
+            ''
+            #!/usr/bin/env sh
+            if [ $# != 3 ]; then
+                echo "usage: $0 <cache-fs> <backing-pool> <days-old>"
+                exit 1
+            fi
+
+            CACHE="${1}"
+            BACKING="${2}"
+            N=${3}
+
+            find "${CACHE}" -type f -atime +${N} -printf '%P\n' | \
+                rsync --files-from=- -axqHAXWES --preallocate --remove-source-files "${CACHE}/" "${BACKING}/"
+            ''
+        )
+        (
+            writeShellScriptBin "mergerfs_percentage_based_mover"
+            ''
+            #!/usr/bin/env sh
+            if [ $# != 3 ]; then
+                echo "usage: $0 <cache-fs> <backing-pool> <percentage>"
+                exit 1
+            fi
+
+            CACHE="${1}"
+            BACKING="${2}"
+            PERCENTAGE=${3}
+
+            set -o errexit
+            while [ $(df --output=pcent "${CACHE}" | grep -v Use | cut -d'%' -f1) -gt ${PERCENTAGE} ]
+            do
+                FILE=$(find "${CACHE}" -type f -printf '%A@ %P\n' | \
+                              sort | \
+                              head -n 1 | \
+                              cut -d' ' -f2-)
+                test -n "${FILE}"
+                rsync -axqHAXWESR --preallocate --relative --remove-source-files "${CACHE}/./${FILE}" "${BACKING}/"
+            done
+            ''
+        )
+    ];
+
+    services.cron = {
+        enable = true;
+        systemCronJobs = [
+            "0 4 */7 * * mergerfs_time_based_mover /mnt/cache /cacheddata 30" # atime based mover every week
+            "0 4 */7 * * mergerfs_percentage_based_mover /mnt/cache /cacheddata 90" # percentage full based mover every week
         ];
     };
 }
