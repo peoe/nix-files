3f41b8eae2b58e1366561f565cc4286a778551b4
Peter Oehme <oehme.pb@gmail.com>
2025-07-14 14:39:50 +0200

    [data] add noatime option

--- packages/datadisks.nix
+++ packages/datadisks.nix
@@ -16,10 +16,18 @@
     fileSystems."/mnt/disks/data1" = {
         device = "/dev/mapper/data1";
         fsType = "ext4";
+        options = [
+            "defaults"
+            "noatime"
+        ];
     };
     fileSystems."/mnt/cache" = {
         device = "/dev/mapper/cache";
         fsType = "ext4";
+        options = [
+            "defaults"
+            "noatime"
+        ];
     };
 
     # mergerfs filesystems for caching
@@ -53,5 +61,9 @@
     fileSystems."/mnt/disks/parity1" = {
         device = "/dev/mapper/parity1";
         fsType = "ext4";
+        options = [
+            "defaults"
+            "noatime"
+        ];
     };
 }
