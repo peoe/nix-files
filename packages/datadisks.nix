646bd47c02e83b3bc714a799c79b5c11561e7fe3
Peter Oehme <oehme.pb@gmail.com>
2025-07-14 13:23:55 +0200

    [data] add crypttab for data disks

---
+++ packages/datadisks.nix
@@ -0,0 +1,13 @@
+{
+    environment.etc.crypttab = {
+        mode = "0600";
+        text = ''
+        # <volume-name> <encrypted-device> [key-file] [options]
+        cache /dev/disk/by-label/cache ${config.sops.secrets."luks-file".path}
+
+        data1 /dev/disk/by-label/data1 ${config.sops.secrets."luks-file".path}
+
+        parity1 /dev/disk/by-label/parity1 ${config.sops.secrets."luks-file".path}
+        '';
+    };
+}
