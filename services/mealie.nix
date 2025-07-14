e22a7685445642f7b28d079402e5d93a84c3a4e9
Peter Oehme <oehme.pb@gmail.com>
2025-07-14 17:08:29 +0200

    [mealie] add longer time window without logins

--- services/mealie.nix
+++ services/mealie.nix
@@ -1,12 +1,19 @@
 { ... }: let
     mealieport = 3002;
 in {
+    networking = {
+        firewall = {
+            allowedTCPPorts = [ mealieport 80 443 ];
+        };
+    };
+
     services.mealie = {
         enable = true;
         port = mealieport;
         listenAddress = "127.0.0.1";
         settings = {
             ALLOW_SIGNUP = "false";
+            TOKEN_TIME = 336;
         };
     };
 }
