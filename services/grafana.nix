3cdf3ba4541d04bdacde4f5da00182e6a3f92553
Peter Oehme <oehme.pb@gmail.com>
2025-10-31 15:14:26 +0100

    [grafana] try and fix loki conf

--- services/grafana.nix
+++ services/grafana.nix
@@ -64,7 +64,6 @@ in {
                 max_chunk_age = "1h";
                 chunk_target_size = 999999;
                 chunk_retain_period = "30s";
-                max_transfer_retries = 0;
             };
 
             schema_config = {
@@ -99,10 +98,6 @@ in {
                 reject_old_samples_max_age = "168h";
             };
 
-            chunk_store_config = {
-                max_look_back_period = "0s";
-            };
-
             table_manager = {
                 retention_deletes_enabled = false;
                 retention_period = "0s";
@@ -110,7 +105,6 @@ in {
 
             compactor = {
                 working_directory = "/var/lib/loki";
-                shared_store = "filesystem";
                 compactor_ring = {
                     kvstore = {
                         store = "inmemory";
