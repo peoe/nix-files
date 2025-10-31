c4c5a727cb5aaebde85ba2164862f53f8b92cbc1
Peter Oehme <oehme.pb@gmail.com>
2025-10-31 15:08:01 +0100

    [grafana] try and fix loki conf

--- services/grafana.nix
+++ services/grafana.nix
@@ -33,6 +33,12 @@ in {
                     isDefault = true;
                     editable = false;
                 }
+                {
+                    name = "Loki";
+                    type = "loki";
+                    url = "http://127.0.0.1:${toString config.services.loki.configuration.server.http_listen_port}";
+                    editable = false;
+                }
             ];
         };
     };
@@ -43,6 +49,75 @@ in {
         configuration = {
             server.http_listen_port = lokiport;
             auth_enabled = false;
+
+            ingester = {
+                lifecycler = {
+                    address = "127.0.0.1";
+                    ring = {
+                        kvstore = {
+                            store = "inmemory";
+                        };
+                        replication_factor = 1;
+                    };
+                };
+                chunk_idle_period = "1h";
+                max_chunk_age = "1h";
+                chunk_target_size = 999999;
+                chunk_retain_period = "30s";
+                max_transfer_retries = 0;
+            };
+
+            schema_config = {
+                configs = [
+                    {
+                        from = "2022-06-06";
+                        store = "boltdb-shipper";
+                        object_store = "filesystem";
+                        schema = "v11";
+                        index = {
+                            prefix = "index_";
+                            period = "24h";
+                        };
+                    }
+                ];
+            };
+
+            storage_config = {
+                boltdb_shipper = {
+                    active_index_directory = "/var/lib/loki/boltdb-shipper-active";
+                    cache_location = "/var/lib/loki/boltdb-shipper-cache";
+                    cache_ttl = "24h";
+                    shared_store = "filesystem";
+                };
+
+                filesystem = {
+                    directory = "/var/lib/loki/chunks";
+                };
+            };
+
+            limits_config = {
+                reject_old_samples = true;
+                reject_old_samples_max_age = "168h";
+            };
+
+            chunk_store_config = {
+                max_look_back_period = "0s";
+            };
+
+            table_manager = {
+                retention_deletes_enabled = false;
+                retention_period = "0s";
+            };
+
+            compactor = {
+                working_directory = "/var/lib/loki";
+                shared_store = "filesystem";
+                compactor_ring = {
+                    kvstore = {
+                        store = "inmemory";
+                    };
+                };
+            };
         };
     };
 
