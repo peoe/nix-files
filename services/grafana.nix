0c4b20d1cf733bd1466415917db3088dd5d92e90
Peter Oehme <oehme.pb@gmail.com>
2025-10-31 14:56:17 +0100

    [grafana] fix missing semicolon

--- services/grafana.nix
+++ services/grafana.nix
@@ -57,7 +57,7 @@ in {
                 filename = "/tmp/positions.yaml";
             };
             clients = [
-                { url = "http://127.0.0.1:${toString config.services.loki.configuration.server.http_listen_port}/loki/api/v1/push" }
+                { url = "http://127.0.0.1:${toString config.services.loki.configuration.server.http_listen_port}/loki/api/v1/push"; }
             ];
             scrape_configs = [
                 {
